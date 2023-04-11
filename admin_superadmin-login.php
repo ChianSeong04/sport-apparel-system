7a084f3757f755768976fcf9cac91a9994be89d4
ChianSeong04 <1201200637@soffice.mmu.edu.my>
2023-04-11 19:44:12 +0800

    Add logo image and edit admin and superadmin login - Seong

--- admin_superadmin-login.php
+++ admin_superadmin-login.php
@@ -14,29 +14,21 @@
     <input type="checkbox" id="flip">
     <div class="cover">
       <div class="front">
-        <img src="images/frontImg.jpg" alt="">
-        <div class="text">
-          <span class="text-1">Every new friend is a <br> new adventure</span>
-          <span class="text-2">Let's get connected</span>
-        </div>
+        <img src="images/logo.png">
       </div>
       <div class="back">
-        <img class="backImg" src="images/backImg.jpg" alt="">
-        <div class="text">
-          <span class="text-1">Complete miles of journey <br> with one step</span>
-          <span class="text-2">Let's get started</span>
-        </div>
+        <img class="backImg" src="images/logo.png" alt="">
       </div>
     </div>
     <div class="forms">
         <div class="form-content">
           <div class="login-form">
-            <div class="title">Login</div>
+            <div class="title">Admin Login</div>
           <form action="#">
             <div class="input-boxes">
               <div class="input-box">
-                <i class="fas fa-envelope"></i>
-                <input type="text" placeholder="Enter your email" required>
+                <i class="fas fa-user"></i>
+                <input type="text" placeholder="Enter your Admin Id" required>
               </div>
               <div class="input-box">
                 <i class="fas fa-lock"></i>
@@ -46,30 +38,27 @@
               <div class="button input-box">
                 <input type="submit" value="Sumbit">
               </div>
-              <div class="text sign-up-text">Don't have an account? <label for="flip">Sigup now</label></div>
+              <div class="text sign-up-text">You are a superadmin user? <label for="flip">Superadmin</label></div>
             </div>
         </form>
       </div>
         <div class="signup-form">
-          <div class="title">Signup</div>
+          <div class="title">Superadmin Login</div>
         <form action="#">
             <div class="input-boxes">
               <div class="input-box">
                 <i class="fas fa-user"></i>
-                <input type="text" placeholder="Enter your name" required>
-              </div>
-              <div class="input-box">
-                <i class="fas fa-envelope"></i>
-                <input type="text" placeholder="Enter your email" required>
+                <input type="text" placeholder="Enter your Superadmin Id" required>
               </div>
               <div class="input-box">
                 <i class="fas fa-lock"></i>
                 <input type="password" placeholder="Enter your password" required>
               </div>
+              <div class="text"><a href="#">Forgot password?</a></div>
               <div class="button input-box">
                 <input type="submit" value="Sumbit">
               </div>
-              <div class="text sign-up-text">Already have an account? <label for="flip">Login now</label></div>
+              <div class="text sign-up-text">You are an admin user? <label for="flip">Admin</label></div>
             </div>
       </form>
     </div>
