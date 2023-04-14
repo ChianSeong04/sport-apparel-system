d59cd1d67eb13c664e243cbfc03e27f764312c36
ChianSeong04 <1201200637@soffice.mmu.edu.my>
2023-04-14 13:49:20 +0800

    Add forget password page and update admin and superadmin login page

---
+++ admin-forget-password.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="styles/admin_style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   </head>
+<body>
+  <div class="container">
+    <input type="checkbox" id="flip">
+    <div class="cover">
+        <div class="front">
+          <img src="images/logo3.png">
+        </div>
+	</div>
+
+	<div class="forms">
+		<div class="form-content">
+			<div class="login-form">
+			  <div class="title">Forget Password?</div>
+			  <form autocomplete="off">
+				<div class="input-boxes">
+				  <div class="input-box">
+					<i class="fas fa-envelope"></i>
+					<input type="email" placeholder="Enter your email" required>
+				  </div>
+				  <div class="button input-box">
+					<input type="submit" value="Submit">
+				  </div>
+				</div>
+                <div class="text sign-up-text text-color">Remember your password?<a href="admin_superadmin-login.php"><strong>&nbsp;Login</strong></a></div>			  
+            </form>
+			</div>
+		</div>
+    </div>
+  </div>
+</body>
+</html>
