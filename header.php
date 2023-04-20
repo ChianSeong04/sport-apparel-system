536af6947240e75ac77a4dff2bb2e01061fd6e56
LAPTOP-JVAMQ8RE\PoYee <1201200959@student.mmu.edu.my>
2023-04-20 18:05:38 +0800

    edit about us page

--- header.php
+++ header.php
@@ -1,3 +1,26 @@
+<!-- Start Top Nav -->
+    <nav class="navbar navbar-expand-lg bg-dark navbar-light d-none d-lg-block" id="templatemo_nav_top">
+        <div class="container text-light">
+            <div class="w-100 d-flex justify-content-between">
+                <div>
+                    <i class="fa fa-envelope mx-2"></i>
+                    <!--<a class="navbar-sm-brand text-light text-decoration-none" href="mailto:info@company.com">info@company.com</a>-->
+					<a class="navbar-sm-brand text-light text-decoration-non" href="mailto:info@company.com">spartasportapparel@google.com</a>
+                    <i class="fa fa-phone mx-2"></i>
+                    <!--<a class="navbar-sm-brand text-light text-decoration-none" href="tel:010-020-0340">010-020-0340</a>-->
+					<a class="navbar-sm-brand text-light text-decoration-none" href="tel:010-020-0340">016-123 4567</a>
+                </div>
+                <div>
+                    <a class="text-light" href="https://fb.com/templatemo" target="_blank" rel="sponsored"><i class="fab fa-facebook-f fa-sm fa-fw me-2"></i></a>
+                    <a class="text-light" href="https://www.instagram.com/" target="_blank"><i class="fab fa-instagram fa-sm fa-fw me-2"></i></a>
+                    <!--<a class="text-light" href="https://twitter.com/" target="_blank"><i class="fab fa-twitter fa-sm fa-fw me-2"></i></a>
+                    <a class="text-light" href="https://www.linkedin.com/" target="_blank"><i class="fab fa-linkedin fa-sm fa-fw"></i></a>-->
+                </div>
+            </div>
+        </div>
+    </nav>
+    <!-- Close Top Nav -->
+
 <!--top header-->
 <link rel="stylesheet" href="assets/css/templatemo.css">
 <nav class="navbar navbar-expand-lg navbar-light shadow">
@@ -19,7 +42,7 @@
                             <a class="nav-link" href="index.php">Home</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="about.php">About</a>
+                            <a class="nav-link" href="about.php">About Us</a>
                         </li>
 						<ul class="dropdown">
                         <li class="nav-item">
