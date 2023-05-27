e822d14a87f4644fc5ca58c272e27c6bc1b82d0c
ChianSeong04 <1201200637@soffice.mmu.edu.my>
2023-05-28 00:10:31 +0800

    edit admin index

--- admin_index.php
+++ admin_index.php
@@ -3,12 +3,12 @@
 
 <head>
 	<title>Admin Dashboard</title>
-
 	<link rel="stylesheet" href="assets/admin_styles/bootstrap.min.css">
 	<link rel="stylesheet" href="assets/admin_styles/fontawesome/css/fontawesome.min.css">
 	<link rel="stylesheet" href="assets/admin_styles/fontawesome/css/all.min.css">
-	<link rel="stylesheet" href="assets/admin_styles/style.css"> </head> 
-	</head>
+	<link rel="stylesheet" href="assets/admin_styles/style.css">
+	<link rel="stylesheet" href="assets/admin_styles/datatables/datatables.min.css" />
+</head>
 
 <body>
 	<div class="main-wrapper">
@@ -16,13 +16,10 @@
 			<div class="header-left">
 				<a href="admin_index.php" class="logo"> <img src="images/logo4.png" alt="logo"> <span class="logoclass">Sparta Sport Apparel</span> </a>
 			</div>
-			
 			<a href="admin_logout.php" class="logout" >Log Out</a>
-
 <?php
 		session_start();
 		include("session_connect.php");
-		
 		if(!isset($_SESSION['id']))
 		{
 			?>
@@ -34,118 +31,116 @@
 			exit();
 		}
 
-?>
+?>	
+	</div>
+	<div class="sidebar" id="sidebar">
+		<div id="sidebar-menu" class="sidebar-menu">
+			<ul>
+				<li style="background-color:grey;">
+					<a href="admin_index.php"><i class="fas fa-tachometer-alt"></i> <span>Admin Dashboard</span></a>
+				</li>
+
+				<li class="list-divider"></li>
+				<li class="submenu" >
+					<a href="#"><i class="fas fa-user"></i> <span> Customer </span> <span class="menu-arrow"></span></a>
+					<ul class="submenu_class" style="display: none;">
+						<li><a href="admin_all-customer.php"> All customer </a></li>
+					</ul>
+				</li>
+
+				<li class="submenu">
+					<a href="#"><i class="fas fa-file-invoice"></i><span> Orders </span> <span class="menu-arrow"></span></a>
+					<ul class="submenu_class" style="display: none;">
+						<li><a href="admin_all-order.php">All Orders </a></li>
+						<li><a href="admin_edit-order.php"> Edit Orders </a></li>
+					</ul>
+				</li>
 			
+				<li class="submenu">
+					<a href="#"><i class="fas fa-key"></i> <span> Product Colour</span> <span class="menu-arrow"></span></a>
+					<ul class="submenu_class" style="display: none;">
+						<li><a href="admin_all-product-color.php">All Product Colour</a></li>
+						<li><a href="admin_add-product-color.php">Add Product Colour</a></li>
+						<li><a href="admin_edit-product-color.php">Edit Product Colour</a></li>
+					</ul>
+				</li>
+
+				<li class="submenu">
+					<a href="#"><i class="fas fa-key"></i> <span> Product Type</span> <span class="menu-arrow"></span></a>
+					<ul class="submenu_class" style="display: none;">
+						<li><a href="admin_all-product-type.php">All Product Type</a></li>
+						<li><a href="admin_add-product-type.php">Add Product Type</a></li>
+						<li><a href="admin_edit-product-type.php">Edit Product Type</a></li>
+					</ul>
+				</li>
+
+				<li class="submenu">
+					<a href="#"><i class="fas fa-key"></i> <span> Product Brand</span> <span class="menu-arrow"></span></a>
+					<ul class="submenu_class" style="display: none;">
+						<li><a href="admin_all-product-brand.php">All Product Brand</a></li>
+						<li><a href="admin_add-product-brand.php">Add Product Brand</a></li>
+						<li><a href="admin_edit-product-brand.php">Edit Product Brand</a></li>
+					</ul>
+				</li>
+
+				<li class="submenu" >
+					<a href="#"><i class="fas fa-key"></i> <span> Product </span> <span class="menu-arrow"></span></a>
+					<ul class="submenu_class" style="display: none;">
+						<li><a href="admin_all-product.php">All Product </a></li>
+						<li><a href="admin_add-product.php">Add Product</a></li>
+						<li><a href="admin_edit-product.php">Edit Product</a></li>
+					</ul>
+				</li>
+
+				<li class="submenu">
+					<a href="#"><i class="far fa-money-bill-alt"></i> <span> Product Sales </span> <span class="menu-arrow"></span></a>
+					<ul class="submenu_class" style="display: none;">
+						<li><a href="admin_invoice-reports.php">Sales Report </a></li>
+					</ul>
+				</li>
+
+				<li class="submenu">
+					<a href="#"><i class="fas fa-cog"></i><span> Profile </span> <span class="menu-arrow"></span></a>
+					<ul class="submenu_class" style="display: none;">
+						<li><a href="admin_profile.php">Profile Setting </a></li>
+					</ul>
+				</li>
+			</ul>
 		</div>
-<div class="sidebar" id="sidebar">
-
-<div id="sidebar-menu" class="sidebar-menu">
-<ul>
-<li style="background-color:grey;">
-<a href="admin_index.php"><i class="fas fa-tachometer-alt"></i> <span>Admin Dashboard</span></a>
-</li>
-<li class="list-divider"></li>
-<li class="submenu" >
-<a href="#"><i class="fas fa-user"></i> <span> Customer </span> <span class="menu-arrow"></span></a>
-<ul class="submenu_class" style="display: none;">
-<li><a href="admin_all-customer.php"> All customer </a></li>
-</ul>
-</li>
-
-<li class="submenu">
-<a href="#"><i class="fas fa-file-invoice"></i><span> Orders </span> <span class="menu-arrow"></span></a>
-<ul class="submenu_class" style="display: none;">
-<li><a href="admin_all-order.php">All Orders </a></li>
-<li><a href="admin_edit-order.php"> Edit Orders </a></li>
-</ul>
-</li>
- 
-<li class="submenu">
-<a href="#"><i class="fas fa-key"></i> <span> Product Colour</span> <span class="menu-arrow"></span></a>
-<ul class="submenu_class" style="display: none;">
-<li><a href="admin_all-product-color.php">All Product Colour</a></li>
-<li><a href="admin_add-product-color.php">Add Product Colour</a></li>
-<li><a href="admin_edit-product-color.php">Edit Product Colour</a></li>
-</ul>
-</li>
-
-<li class="submenu">
-<a href="#"><i class="fas fa-key"></i> <span> Product Type</span> <span class="menu-arrow"></span></a>
-<ul class="submenu_class" style="display: none;">
-<li><a href="admin_all-product-type.php">All Product Type</a></li>
-<li><a href="admin_add-product-type.php">Add Product Type</a></li>
-<li><a href="admin_edit-product-type.php">Edit Product Type</a></li>
-</ul>
-</li>
-
-<li class="submenu">
-<a href="#"><i class="fas fa-key"></i> <span> Product Brand</span> <span class="menu-arrow"></span></a>
-<ul class="submenu_class" style="display: none;">
-<li><a href="admin_all-product-brand.php">All Product Brand</a></li>
-<li><a href="admin_add-product-brand.php">Add Product Brand</a></li>
-<li><a href="admin_edit-product-brand.php">Edit Product Brand</a></li>
-</ul>
-</li>
-
-<li class="submenu" >
-<a href="#"><i class="fas fa-key"></i> <span> Product </span> <span class="menu-arrow"></span></a>
-<ul class="submenu_class" style="display: none;">
-<li><a href="admin_all-product.php">All Product </a></li>
-<li><a href="admin_add-product.php">Add Product</a></li>
-<li><a href="admin_edit-product.php">Edit Product</a></li>
-</ul>
-</li>
-
-<li class="submenu">
-<a href="#"><i class="far fa-money-bill-alt"></i> <span> Product Sales </span> <span class="menu-arrow"></span></a>
-<ul class="submenu_class" style="display: none;">
-<li><a href="admin_invoice-reports.php">Sales Report </a></li>
-</ul>
-</li>
-
-<li class="submenu">
-<a href="#"><i class="fas fa-cog"></i><span> Profile </span> <span class="menu-arrow"></span></a>
-<ul class="submenu_class" style="display: none;">
-<li><a href="admin_profile.php">Profile Setting </a></li>
-</ul>
-</li>
-</ul>
-</div>
-</div>
+	</div>
+	
 		<div class="page-wrapper">
 			<div class="content container-fluid">
 				<div class="page-header">
 					<div class="row">
 						<div class="col-sm-12 mt-5">
 							<h3 class="page-title mt-3">Admin Dashboard</h3>
-							
 						</div>
 					</div>
 				</div>
 				<div class="row">
-					<div class="col-xl-3 col-sm-6 col-12">
+					<div class="col-4">
 						<div class="card board1 fill">
 							<div class="card-body">
 								<div class="dash-widget-header">
 									<div>
 										<h3 class="card_widget_header">
-
 										<?php
 											$result = mysqli_query($connect, "SELECT * FROM customer");	
 											$count = mysqli_num_rows($result);//used to count number of rows
 											echo "$count";
 										?>
 										</h3>
-										<h6 class="text-muted">Total Customer</h6> </div>
-									
+										<h5 class="text-muted">Total Customer</h5> 
+									</div>
 									<div class="ml-auto mt-md-3 mt-lg-0">
-									<i class="fas fa-user-plus fa-2x"></i>
-									 </div>
+										<i class="fas fa-user-plus fa-2x"></i>
+									</div>
 								</div>
 							</div>
 						</div>
 					</div>
-					<div class="col-xl-3 col-sm-6 col-12">
+					<div class="col-4">
 						<div class="card board1 fill">
 							<div class="card-body">
 								<div class="dash-widget-header">
@@ -157,15 +152,15 @@
 											echo "$count";
 										?>
 										</h3>
-										<h6 class="text-muted">Total Orders</h6> </div>
-									
+										<h5 class="text-muted">Total Orders</h5>
+									</div>
 									<div class="ml-auto mt-md-3 mt-lg-0"> <i class="fas fa-shopping-basket fa-2x"></i></div>
 								</div>
 							</div>
 						</div>
 					</div>
 					
-					<div class="col-xl-3 col-sm-6 col-12">
+					<div class="col-4">
 						<div class="card board1 fill">
 							<div class="card-body">
 								<div class="dash-widget-header">
@@ -177,76 +172,68 @@
 											echo "$count";
 										?>
 										</h3>
-										<h6 class="text-muted">Total Product</h6> </div>
+										<h5 class="text-muted">Total Product</h5>
+									</div>
 									<div class="ml-auto mt-md-3 mt-lg-0"> <i class="fas fa-globe fa-2x"></i> </div>
 								</div>
 							</div>
 						</div>
 					</div>
 				</div>
-				
-				<div class="row">
-					<div class="col-md-12 d-flex">
-						<div class="card card-table flex-fill">
-							<div class="card-header">
-								<h3 class="page-title float-left mt-2">Order</h3>
-								<a href="admin_all-order.php" class="mt-2 btn btn-primary float-right veiwbutton">View All</a>
-							</div>
-							<div class="card-body">
-								<div class="table-responsive">
-									<table class="table table-hover table-center">
-										<thead style="text-align:center;">
-											<tr>
-											<th>Order ID</th>
-												<th>Customer ID</th>
-												<th>Product ID</th>
-												<th>Quantity</th>
-												<th>Order Subtotal</th>
-												<th>Payment Method</th>
-												<th>Grandtotal</th>
-												<th>Order Date</th>
-												<th>Status</th>
-											</tr>
-										</thead>
-										<tbody>
-										<?php 
-										$result = mysqli_query($connect, "SELECT * from cart 
-										JOIN customer_order ON customer_order.order_id = cart.payment_status
-										JOIN payment ON customer_order.payment_id=payment.payment_id WHERE cart.payment_status!=0");
-										while($row = mysqli_fetch_assoc($result))
-										{
-								?>
-											<tr>
-												<td style="text-align:center;"><?php echo $row['order_id'] ?></td>
-												<td style="text-align:center;"><?php echo $row['customer_id'] ?></td>
-												<td style="text-align:center;"><?php echo $row['product_id'] ?></td>
-												<td style="text-align:center;"><?php echo $row['product_quantity'] ?></td>
-												<td style="text-align:center;">RM <?php echo $row['cart_subtotal'] ?></td>
-												<td style="text-align:center;"><?php echo $row['payment_type'] ?></td>
-												<td style="text-align:center;">RM <?php echo $row['grandtotal'] ?></td>
-												<td style="text-align:center;"><?php echo $row['order_date'] ?></td>
-												<td style="text-align:center;"><?php echo $row['order_status'] ?></td>
-												
-											</tr>
-								<?php
-										}
-								?>
-										</tbody>
-									</table>
-								</div>
-							</div>
-						</div>
+					<h3 class="ml-3">Order</h3>
+					<div>
+						<table id="index_table" class="table table-striped table-bordered table-hover ml-3">
+							<thead>
+								<th>Order ID</th>
+								<th>Customer ID</th>
+								<th>Product ID</th>
+								<th>Quantity</th>
+								<th>Order Subtotal</th>
+								<th>Payment Method</th>
+								<th>Grandtotal</th>
+								<th>Order Date</th>
+								<th>Status</th>
+							</thead>
+
+							<tbody>
+							<?php 
+								$result = mysqli_query($connect, "SELECT * from cart 
+								JOIN customer_order ON customer_order.order_id = cart.payment_status
+								JOIN payment ON customer_order.payment_id=payment.payment_id WHERE cart.payment_status!=0");
+								while($row = mysqli_fetch_assoc($result))
+									{
+							?>
+								<tr>
+									<td style="text-align:center;"><?php echo $row['order_id'] ?></td>
+									<td style="text-align:center;"><?php echo $row['customer_id'] ?></td>
+									<td style="text-align:center;"><?php echo $row['product_id'] ?></td>
+									<td style="text-align:center;"><?php echo $row['product_quantity'] ?></td>
+									<td style="text-align:center;">RM <?php echo $row['cart_subtotal'] ?></td>
+									<td style="text-align:center;"><?php echo $row['payment_type'] ?></td>
+									<td style="text-align:center;">RM <?php echo $row['grandtotal'] ?></td>
+									<td style="text-align:center;"><?php echo $row['order_date'] ?></td>
+									<td style="text-align:center;"><?php echo $row['order_status'] ?></td>
+								</tr>
+							<?php
+									}
+							?>
+							</tbody>
+						</table>
 					</div>
-				</div>
 			</div>
 		</div>
 	</div>
-<!--elective choosen navigate bar-->
+
+
+<!--Script-->
 	<script src="assets/admin_styles/jquery-3.5.1.min.js"></script>
-	<script src="assets/admin_styles/raphael/raphael.min.js"></script>
-	<script src="assets/admin_styles/morris/morris.min.js"></script>
-	<script src="assets/admin_styles/chart.morris.js"></script>
 	<script src="assets/admin_styles/script.js"></script>
+	<script src="assets/admin_styles/datatables/datatables.min.js"></script>
+	<script>
+		$(document).ready(function () {
+			$('#index_table').DataTable();
+		});
+	</script>
 </body>
 
 </html>
