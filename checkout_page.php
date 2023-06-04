2afd2ba15a1a145dcde21529fda4aadc7022ddab
GohYuHengGYH <1201200443@student.mmu.edu.my>
2023-06-04 15:48:47 +0800

    add the php update database but still cannot work properly yet

--- checkout_page.php
+++ checkout_page.php
@@ -208,7 +208,17 @@ session_start();
   }
 </style>
 
+<?php
+$order_date = date("Y-m-d");
+$status_order = "Paid Out";
 
+if(isset($_POST["order_btn"]))
+{
+	$payment_type=mysqli_query($connect,"INSERT INTO payment(payment_type,grandtotal) VALUES ('Credit Card','$gtt')");
+	$create_order=mysqli_query($connect,"INSERT INTO customer_order (customer_id,order_status,payment_id,order_date) VALUES ('$cusid','$status_order',LAST_INSERT_ID(),'$order_date')");
+	$update_cart=mysqli_query($connect,"UPDATE cart SET payment_status=LAST_INSERT_ID() WHERE customer_id='$cusid' AND payment_status=0");
+}
+?>
 
 </body>
 
